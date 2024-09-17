f099adff2541e749e22956b502ea31533dc2a993
glebfra <gleb.frantsuzov@yandex.ru>
2024-09-17 20:00:09 +0400

    Final commit

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,6 @@ import {Button, Card} from "react-bootstrap";
 import axios from "axios";
 
 function ProductCard({id, title, image, price, userProducts = []}) {
-    const [isOnCart, setIsOnCart] = useState(false);
     const [isLogged, setIsLogged] = useState(localStorage.getItem('token') != null);
 
     const addOnCart = () => {
@@ -24,24 +23,6 @@ function ProductCard({id, title, image, price, userProducts = []}) {
         })
     }
 
-    const deleteFromCart = () => {
-        axios.post(
-            'http://localhost:1337/api/cart/delete',
-            {
-                id: id
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('token')}`
-                }
-            }
-        ).then(response => {
-            console.log(response);
-        }).catch(error => {
-            console.log(error);
-        })
-    }
-
     return (
         <Card className='d-flex align-items-center' style={{width: '18rem'}}>
             <Card.Title className='m-3'>{title}</Card.Title>
@@ -50,13 +31,7 @@ function ProductCard({id, title, image, price, userProducts = []}) {
                 <Card.Text>Price: {price}</Card.Text>
             </Card.Body>
             {isLogged ? (
-                isOnCart ? (
-                    <Button variant='outline-danger' className='d-flex p-3 mb-3' onClick={deleteFromCart}>Delete from
-                            cart</Button>
-                ) : (
-                    <Button variant='outline-success' className='d-flex p-3 mb-3' onClick={addOnCart}>Add to
-                            cart</Button>
-                )
+                <Button variant='outline-success' className='d-flex p-3 mb-3' onClick={addOnCart}>Add to cart</Button>
             ) : (
                 <Button variant='outline-success' className='d-flex p-3 mb-3' href='/login/'>Login</Button>
             )}
